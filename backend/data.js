4dc03b7b42eea3936197e9c08e2dab9078b9ada5
petermagalong0421@gmail.com <petermagalong0421@gmail.com>
2022-03-05 02:09:47 -0800

    first commit

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products:[
+        {
+            name:'Nike Slim Shirt',
+            slug: 'nike-sling-shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            name:'Adidas Fit Shirt',
+            slug: 'adidas-fit-shirt',
+            category: 'Shirts',
+            image:'/images/p2.jpg',
+            price: 250,
+            countInStock: 20,
+            brand: 'Adida',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            name:'Nike Slim Pant',
+            slug: 'nike-sling-pant',
+            category: 'Pants',
+            image:'/images/p3.jpg',
+            price: 25,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'high quality shirt'
+        },
+        {
+            name:'Adidas Slim Pant',
+            slug: 'Adidas-sling-pant',
+            category: 'Pants',
+            image:'/images/p4.jpg',
+            price: 65,
+            countInStock: 5,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+    ]
+}
+
+export default data
